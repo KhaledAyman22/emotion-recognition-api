330acefd77dfeacb74be8b295e263211fbd8e80b
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2022-06-27 20:34:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,24 +26,6 @@ class Item(BaseModel):
 model = keras.models.load_model('my_arch64.h5')
 
 
-async def FetchFace(img):
-    img = np.array(img)
-    fc = cv.CascadeClassifier(r'haarcascade_frontalface_default.xml')
-    detection_result, rejectLevels, levelWeights = fc.detectMultiScale3(img, outputRejectLevels=True,
-                                                                        scaleFactor=1.1, minNeighbors=3,
-                                                                        minSize=(48, 48))
-    try:
-        levelWeights = levelWeights.reshape(len(levelWeights))
-        max_index = list(levelWeights).index(levelWeights.max())
-        x, y, w, h = detection_result[max_index]
-
-        face = img[y:y + h, x:x + w]
-        gray = cv.cvtColor(face, cv.COLOR_BGR2GRAY)
-        return cv.resize(gray, (128, 128))
-    except:
-        return -1
-
-
 @app.post('/')
 async def GetMood(item: Item):
     b = base64.b64decode(item.img)
